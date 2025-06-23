55109fbb4ea95edc97da83f421ef42c42621c1b3
Evanildo Silva Ferreira <evanildo.ferreira@livsaude.com.br>
2025-06-23 15:34:04 -0300

    feat: create agent form

---
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -0,0 +1,129 @@
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+import { AgentGetOne } from "../../types";
+import { agentsInsertSchema, AgentsInsertType } from "../../schemas";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+interface AgentFormProps {
+  onSuccess?: VoidFunction;
+  onCancel?: VoidFunction;
+  initValues?: AgentGetOne;
+}
+
+const AgentForm = ({ onSuccess, onCancel, initValues }: AgentFormProps) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  const createAgent = useMutation(
+    trpc.agents.create.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions());
+
+        if (initValues?.id) {
+          await queryClient.invalidateQueries(
+            trpc.agents.getOne.queryOptions({ id: initValues.id })
+          );
+        }
+        onSuccess?.();
+      },
+      onError: (error) => {
+        toast.error(error.message);
+
+        //TODO: Check if error code is "FORBIDDEN", redirect to "/upgrade"
+      },
+    })
+  );
+
+  const form = useForm<AgentsInsertType>({
+    resolver: zodResolver(agentsInsertSchema),
+    defaultValues: {
+      name: initValues?.name ?? "",
+      instructions: initValues?.instructions ?? "",
+    },
+  });
+
+  const isEdit = !!initValues?.id;
+  // const isPedding = createAgent.isPending || updateAgent.isPending;
+  const isPedding = createAgent.isPending;
+
+  const onSubmit = (values: AgentsInsertType) => {
+    if (isEdit) {
+      console.log("🚀 ~ TODO: updateAgent ~");
+    } else {
+      createAgent.mutate(values);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <GeneratedAvatar
+          seed={form.watch("name")}
+          variant="botttsNeutral"
+          className="border size-16"
+        />
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="e.g. Prof. de Matemática" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="instructions"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Instruções</FormLabel>
+              <FormControl>
+                <Textarea
+                  {...field}
+                  placeholder="Você é um matemático prestativo que pode responder perguntas e ajudar com tarefas"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-between gap-x-2">
+          {onCancel && (
+            <Button
+              variant="ghost"
+              disabled={isPedding}
+              type="button"
+              onClick={onCancel}
+            >
+              Cancelar
+            </Button>
+          )}
+          <Button disabled={isPedding} type="submit">
+            {isEdit ? "Atualizar" : "Criar"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export { AgentForm };
