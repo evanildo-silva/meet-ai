ef38e6f40c834edf1e0f5852f17b84cafe190b4b
Evanildo Silva Ferreira <evanildo.ferreira@livsaude.com.br>
2025-06-23 15:29:21 -0300

    refactor: add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/client";
 
+import { Toaster } from "@/components/ui/sonner";
+
 import "./globals.css";
 
 const inter = Inter({
@@ -23,7 +25,10 @@ export default function RootLayout({
   return (
     <TRPCReactProvider>
       <html lang="en">
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <body className={`${inter.className} antialiased`}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </TRPCReactProvider>
   );
