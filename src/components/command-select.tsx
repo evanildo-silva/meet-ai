a8fea40c80065e3b521e9ef844408a028061a340
Evanildo Silva Ferreira <evanildo.ferreira@livsaude.com.br>
2025-06-26 13:44:15 -0300

    feat: create meeting command

---
+++ src/components/command-select.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import { ChevronsUpDownIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandResponsiveDialog,
+} from "@/components/ui/command";
+
+interface CommandSelectProps {
+  options: Array<{
+    id: string;
+    value: string;
+    children: ReactNode;
+  }>;
+  onSelect: (value: string) => void;
+  onSearch?: (value: string) => void;
+  value: string;
+  placeholder?: string;
+  isSearching?: boolean;
+  className?: string;
+}
+
+const CommandSelect = ({
+  options,
+  value,
+  placeholder,
+  className,
+  onSelect,
+  onSearch,
+}: CommandSelectProps) => {
+  const [open, setOpen] = useState(false);
+  const selectedOption = options.find((option) => option.value === value);
+
+  return (
+    <>
+      <Button
+        type="button"
+        variant="outline"
+        onClick={() => setOpen(true)}
+        className={cn(
+          "h-9 justify-between font-normal px-2",
+          !selectedOption && "text-muted-foreground",
+          className
+        )}
+      >
+        <div>{selectedOption?.children ?? placeholder}</div>
+        <ChevronsUpDownIcon />
+      </Button>
+      <CommandResponsiveDialog shouldFilter={!onSearch} open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Buscar por..." onValueChange={onSearch} />
+        <CommandList>
+          <CommandEmpty className="text-muted-foreground text-sm text-center">
+            <span>Opção não encontarda</span>
+          </CommandEmpty>
+          {options.map((option) => (
+            <CommandItem
+              key={option.id}
+              onSelect={() => {
+                onSelect(option.value);
+                setOpen(false);
+              }}
+            >
+              {option.children}
+            </CommandItem>
+          ))}
+        </CommandList>
+      </CommandResponsiveDialog>
+    </>
+  );
+};
+
+export { CommandSelect };
