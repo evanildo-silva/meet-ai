073a9449df3bfd782337c426a36dec3ec46c8c01
Evanildo Silva Ferreira <evanildo.ferreira@livsaude.com.br>
2025-06-26 16:15:26 -0300

    fix: command select filter reset

--- src/components/command-select.tsx
+++ src/components/command-select.tsx
@@ -38,6 +38,11 @@ const CommandSelect = ({
   const [open, setOpen] = useState(false);
   const selectedOption = options.find((option) => option.value === value);
 
+  const handleOpenChange = (open: boolean) => {
+    onSearch?.("");
+    setOpen(open);
+  };
+
   return (
     <>
       <Button
@@ -53,7 +58,11 @@ const CommandSelect = ({
         <div>{selectedOption?.children ?? placeholder}</div>
         <ChevronsUpDownIcon />
       </Button>
-      <CommandResponsiveDialog shouldFilter={!onSearch} open={open} onOpenChange={setOpen}>
+      <CommandResponsiveDialog
+        shouldFilter={!onSearch}
+        open={open}
+        onOpenChange={handleOpenChange}
+      >
         <CommandInput placeholder="Buscar por..." onValueChange={onSearch} />
         <CommandList>
           <CommandEmpty className="text-muted-foreground text-sm text-center">
