70e960b245e2c35170db89f05ade3f24543161ad
Evanildo Silva Ferreira <evanildo.ferreira@livsaude.com.br>
2025-06-27 10:14:46 -0300

    refactor: add meeting filters

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -4,17 +4,22 @@ import { useRouter } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filters";
 
+import { columns } from "../components/columns";
+import { DataTable } from "@/components/data-table";
+import { EmptyState } from "@/components/empty-state";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
-import { DataTable } from "@/components/data-table";
-import { columns } from "../components/columns";
-import { EmptyState } from "@/components/empty-state";
+import { DataPagination } from "@/components/data-pagination";
 
 const MeetingsView = () => {
+  const [filters, setFilters] = useMeetingsFilters();
   const router = useRouter();
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+  const { data } = useSuspenseQuery(
+    trpc.meetings.getMany.queryOptions({ ...filters })
+  );
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
@@ -22,6 +27,11 @@ const MeetingsView = () => {
         columns={columns}
         data={data.items}
         onClickRow={(row) => router.push(`/meetings/${row.id}`)}
+      />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
       />
       {data.items.length === 0 && (
         <EmptyState
